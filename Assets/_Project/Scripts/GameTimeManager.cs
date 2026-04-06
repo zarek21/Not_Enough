3e0f25d450d056e8b4d3f6fa711865daf77e5492
Zarek Saleme <jorge.saleme77@gmail.com>
2026-04-06 15:51:42 -0600

    Fixed level and re-applied FPS cap (80 FPS)

--- Assets/_Project/Scripts/GameTimeManager.cs
+++ Assets/_Project/Scripts/GameTimeManager.cs
@@ -4,6 +4,13 @@ namespace NotEnough.Systems
 {
     public class GameTimeManager : MonoBehaviour
     {
-        
+        private void Awake()
+        {
+            // Limitamos a 80 FPS
+            Application.targetFrameRate = 80;
+            
+            // Desactivamos VSync para que el target sea exacto
+            QualitySettings.vSyncCount = 0;
+        }
     }
 }
